1c433cf910e7f17e7a6d2ab045616686eddc23b8
sevorl <sebastian.vorlaender@gmail.com>
2026-01-19 16:47:32 +0100

    Enable grouped condition icons in simple mode

--- src/components/wfc-forecast-simple.ts
+++ src/components/wfc-forecast-simple.ts
@@ -8,7 +8,7 @@ import {
   ForecastActionDetails,
   WeatherForecastCardConfig,
 } from "../types";
-import { formatDay } from "../helpers";
+import { formatDay, groupForecastByCondition } from "../helpers";
 import {
   ForecastAttribute,
   ForecastType,
@@ -44,6 +44,7 @@ export class WfcForecastSimple extends LitElement {
       return nothing;
     }
 
+    const useGroupedIcons = this.config.forecast?.group_condition_icons ?? false;
     const forecastTemplates: TemplateResult[] = [];
     const maxPrecipitation = getMaxPrecipitationForUnit(
       getWeatherUnit(this.hass, this.weatherEntity, "precipitation"),
@@ -52,46 +53,120 @@ export class WfcForecastSimple extends LitElement {
 
     let currentDay: string | undefined;
 
-    this.forecast.forEach((forecast, index) => {
-      if (!forecast.datetime) {
-        return;
-      }
+    if (useGroupedIcons && this.forecastType === "hourly") {
+      const timeRow: TemplateResult[] = [];
+      const spanRow: TemplateResult[] = [];
+      const detailRow: TemplateResult[] = [];
+      const conditionSpans = groupForecastByCondition(this.forecast, this.hass);
 
-      if (this.forecastType === "hourly") {
-        const forecastDay = formatDay(this.hass, forecast.datetime);
-        if (currentDay !== forecastDay) {
-          currentDay = forecastDay;
-          forecastTemplates.push(
-            html`<div class="wfc-day-indicator-container">
-              <div class="wfc-day-indicator wfc-label">${forecastDay}</div>
-            </div>`
-          );
+      this.forecast.forEach((forecast, index) => {
+        if (!forecast.datetime) {
+          return;
         }
-      }
+
+        // Time labels only
+        timeRow.push(html`
+          <div class="wfc-forecast-slot" data-index=${index}>
+            <wfc-forecast-header-items
+              .hass=${this.hass}
+              .forecast=${forecast}
+              .forecastType=${this.forecastType}
+              .config=${this.config}
+              .hideIcon=${true}
+              .hideTime=${false}
+            ></wfc-forecast-header-items>
+          </div>
+        `);
+
+        // Condition spans
+        const conditionSpan = conditionSpans.find(span => span.startIndex === index);
+        if (conditionSpan) {
+          const spanWidth = `calc(${conditionSpan.count} * var(--forecast-item-width) + ${conditionSpan.count - 1} * var(--forecast-item-gap))`;
+          spanRow.push(html`
+            <div 
+              class="wfc-forecast-condition-span" 
+              style="width: ${spanWidth}; grid-column: span ${conditionSpan.count};"
+            >
+              <wfc-forecast-header-items
+                .hass=${this.hass}
+                .forecast=${forecast}
+                .forecastType=${this.forecastType}
+                .config=${this.config}
+                .hideTime=${true}
+                .hideIcon=${false}
+              ></wfc-forecast-header-items>
+            </div>
+          `);
+        }
+
+        // Details and info per slot
+        detailRow.push(html`
+          <div class="wfc-forecast-slot" data-index=${index}>
+            <wfc-forecast-details
+              .hass=${this.hass}
+              .forecast=${forecast}
+              .maxPrecipitation=${maxPrecipitation}
+              .config=${this.config}
+            ></wfc-forecast-details>
+            <wfc-forecast-info
+              .hass=${this.hass}
+              .weatherEntity=${this.weatherEntity}
+              .forecast=${forecast}
+              .config=${this.config}
+            ></wfc-forecast-info>
+          </div>
+        `);
+      });
 
       forecastTemplates.push(html`
-        <div class="wfc-forecast-slot" data-index=${index}>
-          <wfc-forecast-header-items
-            .hass=${this.hass}
-            .forecast=${forecast}
-            .forecastType=${this.forecastType}
-            .config=${this.config}
-          ></wfc-forecast-header-items>
-          <wfc-forecast-details
-            .hass=${this.hass}
-            .forecast=${forecast}
-            .maxPrecipitation=${maxPrecipitation}
-            .config=${this.config}
-          ></wfc-forecast-details>
-          <wfc-forecast-info
-            .hass=${this.hass}
-            .weatherEntity=${this.weatherEntity}
-            .forecast=${forecast}
-            .config=${this.config}
-          ></wfc-forecast-info>
+        <div class="wfc-forecast-grouped-wrapper">
+          <div class="wfc-forecast-time-row">${timeRow}</div>
+          <div class="wfc-forecast-span-row">${spanRow}</div>
+          <div class="wfc-forecast-row">${detailRow}</div>
         </div>
       `);
-    });
+    } else {
+      this.forecast.forEach((forecast, index) => {
+        if (!forecast.datetime) {
+          return;
+        }
+
+        if (this.forecastType === "hourly") {
+          const forecastDay = formatDay(this.hass, forecast.datetime);
+          if (currentDay !== forecastDay) {
+            currentDay = forecastDay;
+            forecastTemplates.push(
+              html`<div class="wfc-day-indicator-container">
+                <div class="wfc-day-indicator wfc-label">${forecastDay}</div>
+              </div>`
+            );
+          }
+        }
+
+        forecastTemplates.push(html`
+          <div class="wfc-forecast-slot" data-index=${index}>
+            <wfc-forecast-header-items
+              .hass=${this.hass}
+              .forecast=${forecast}
+              .forecastType=${this.forecastType}
+              .config=${this.config}
+            ></wfc-forecast-header-items>
+            <wfc-forecast-details
+              .hass=${this.hass}
+              .forecast=${forecast}
+              .maxPrecipitation=${maxPrecipitation}
+              .config=${this.config}
+            ></wfc-forecast-details>
+            <wfc-forecast-info
+              .hass=${this.hass}
+              .weatherEntity=${this.weatherEntity}
+              .forecast=${forecast}
+              .config=${this.config}
+            ></wfc-forecast-info>
+          </div>
+        `);
+      });
+    }
 
     return html`
       <div
